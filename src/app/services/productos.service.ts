25a41648ce5680398964d4a0deb0523dd8f0ca93
Anastacio Aban Noh <anastacio.9308@gmail.com>
2018-02-08 11:35:38 -0600

    Agregamos firebase a el portafolio

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -18,6 +18,7 @@ export class ProductosService {
     this.http.get('https://paginaweb-1454b.firebaseio.com/productos_idx.json')
       .subscribe(res => {
         console.log(res.json());
+        this.productos = res.json();
         this.cargando = false;
       });
   }
